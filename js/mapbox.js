aecb1bca055a03ee4c3edad2f951cadc0d71ffe7
Peter Forstner <p.forstner@liwest.at>
2022-04-23 20:15:01 +0200

    added marker to mapbox

--- js/mapbox.js
+++ js/mapbox.js
@@ -3,6 +3,62 @@ mapboxgl.accessToken = 'pk.eyJ1IjoicmVhcHo5NyIsImEiOiJja3lxMzc2cnMwZnBiMnBtdjhwd
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/reapz97/ckuh4pwsc56q818rz2xbs4mn5', // style URL
-    center: [14.2526, 48.1890], // starting position [lng, lat]
-    zoom: 14 // starting zoom
+    center: [14.25279, 48.18571], // starting position [lng, lat] 48,18571° N, 14,25279° E
+    zoom: 14.2 // starting zoom
 });
+map.scrollZoom.disable();
+
+
+//MAP MARKER GEODATA
+const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [14.250094, 48.185203] //48.185203, 14.250094
+        },
+        properties: {
+          title: 'Festzelt',
+          description: 'Standort Festzelt'
+        }
+      },
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [14.254946, 48.186700] //48.186700, 14.254946
+        },
+        properties: {
+          title: 'Wertungsplatz',
+          description: 'Standort Wertungsplatz'
+        }
+      }
+    ]
+};
+
+// create the popups
+const popup_marching = new mapboxgl.Popup({ offset: 25 }).setText(
+    'Wertungsplatz'
+    );
+
+const popup_tent = new mapboxgl.Popup({ offset: 25 }).setText(
+    'Festzelt!'
+    );
+
+//add markers
+var el = document.createElement('div');
+el.className = 'marker_tent_3';
+new mapboxgl.Marker(el)
+    .setLngLat(geojson.features[0].geometry.coordinates)
+    .setPopup(popup_tent)
+    .addTo(map);
+
+el = document.createElement('div');
+el.className = 'marker_marching';
+new mapboxgl.Marker(el)
+    .setLngLat(geojson.features[1].geometry.coordinates)
+    .setPopup(popup_marching) // sets a popup on this marker
+    .addTo(map);
+
